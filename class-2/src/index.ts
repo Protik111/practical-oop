ac87edfa17ada22b4f47571c7d9ad02af90864d4
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-12-06 23:48:17 +0600

    feat: Audiobook class

--- class-2/src/index.ts
+++ class-2/src/index.ts
@@ -1,16 +1,79 @@
 import { Book } from "./Book";
+import { Ebook } from "./Ebook";
+import { InventoryManager } from "./InventoryManager";
+import { Magazine } from "./Magazine";
 
-const book = new Book(
-  "b1",
-  "Clean Code",
-  "Robert C. Martin",
-  30,
-  10,
-  "9780132350884",
-  464,
-  "Programming"
+// const book = new Book(
+//   "b1",
+//   "Clean Code",
+//   "Robert C. Martin",
+//   30,
+//   10,
+//   "9780132350884",
+//   464,
+//   "Programming"
+// );
+
+// console.log(book.checkStock(5));
+// console.log(book.addStock(12));
+// console.log(book.stock);
+
+const inventoryManager = new InventoryManager();
+
+inventoryManager.addProduct(
+  new Book(
+    "id1",
+    "First Book",
+    "Test Author",
+    500,
+    10,
+    "asdasdsad",
+    100,
+    "Fiction"
+  )
 );
 
-console.log(book.checkStock(5));
-console.log(book.addStock(12));
-console.log(book.stock);
+inventoryManager.addProduct(
+  new Magazine(
+    "id2",
+    "First Magazine",
+    500,
+    10,
+    1,
+    "Test Publisher",
+    new Date()
+  )
+);
+
+inventoryManager.addProduct(
+  new Ebook(
+    "id3",
+    "Ebook title 1",
+    "Test Author",
+    500,
+    "https://www.google.com"
+  )
+);
+
+inventoryManager.addProduct(
+  new Ebook(
+    "id4",
+    "Ebook title 2",
+    "Test Author",
+    500,
+    "https://www.google.com"
+  )
+);
+
+inventoryManager.addProduct(
+  new Book(
+    "id5",
+    "First Book",
+    "Test Author",
+    500,
+    10,
+    "asdasdsad",
+    100,
+    "Fiction"
+  )
+);
