215a0be9c09a7bffcdeb917a821c6ba44c6d6f0e
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2025-12-06 19:00:32 +0600

    feat: Book class extends

--- class-2/src/index.ts
+++ class-2/src/index.ts
@@ -1,1 +1,16 @@
-console.log('test');
+import { Book } from "./Book";
+
+const book = new Book(
+  'b1',
+  'Clean Code',
+  'Robert C. Martin',
+  30,
+  10,
+  '9780132350884',
+  464,
+  'Programming'
+);
+
+console.log(book.checkStock(5));
+console.log(book.addStock(12));
+console.log(book.stock);
